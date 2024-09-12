33edca0f58429f43a7f4bef9f944951b7e1aaa55
nadinnah <nadinahmedmohamed@gmail.com>
2024-09-12 15:36:27 +0300

    add .env to repository

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
+
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 const db= new pg.Client({
-  host: "localhost",
-  port: 5433,
-  password:"Jun23135",
-  user:"postgres",
-  database:"secrets"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME
 });
 db.connect();
 
